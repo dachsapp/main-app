11b6a65aa7d65d0cbbf631bc68889653be1dad5b
= <=>
2021-04-20 08:58:16 +0200

    serverside works now, problem: 2 commandlines at once

---
+++ src/app/app-service.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppServiceService {
+  constructor(private http: HttpClient) {}
+
+  sendMail = (email: string) => {
+    return this.http.get('/serverside/sendMail');
+  };
+}
